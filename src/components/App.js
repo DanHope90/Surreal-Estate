a7a6e62197744d5b2639e2bf18821a5eb2479679
Dan Hope <dplhope@gmail.com>
2022-04-08 09:26:47 +0100

    routing added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "../styles/App.css";
+import { Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 function App() {
   return (
     <body className="body">
       <div className="App">
         <NavBar />
+        <Switch>
+          <Route exact path="/" component={Properties} />
+          <Route exact path="/add-property" component={AddProperty} />
+        </Switch>
       </div>
     </body>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,18 @@
-import React from "react";
 import "../styles/NavBar.css";
+import { Link, React } from "react-router-dom";
 import logo from "../logo.png";
 
 function NavBar() {
   return (
     <div className="navbar">
       <img className="logo" src={logo} alt="nav-logo" />
-      <ul className="navbar-links">
-        <li className="navbar-links-items">View Properties</li>
-        <li className="navbar-links-items">Add a Property</li>
+      <ul>
+        <Link className="navbar-links-items" to="/">
+          View Properties
+        </Link>
+        <Link className="navbar-links-items" to="/add-property">
+          Add Property
+        </Link>
       </ul>
     </div>
   );
