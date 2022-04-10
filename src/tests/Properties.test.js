825184b643563ad0a0a20ca62e790b40bcf886e6
Dan Hope <dplhope@gmail.com>
2022-04-10 18:04:55 +0100

    tests added

---
+++ src/tests/Properties.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Properties from "../components/Properties";
+
+describe("Properties", () => {
+  const { asFragment } = render(<Properties />);
+
+  it("renders correctly", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
