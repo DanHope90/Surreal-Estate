5ec5ec159bacd6bf3541f3c6a5d9b2e537fe0a91
Dan Hope <dplhope@gmail.com>
2022-04-16 10:50:31 +0100

    SideBar rendered in properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 import { React, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import PropertyCard from "./PropertyCard";
 import Alert from "./Alert";
+import SideBar from "./SideBar";
 import "../styles/Properties.css";
 
 function Properties() {
@@ -27,15 +30,28 @@ function Properties() {
       });
   }, []);
 
+  const { search } = useLocation();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/v1/PropertyListing${search}`)
+      .then(({ data }) => setProperties(data))
+      .catch((err) => console.error(err));
+  }, [search]);
+
   return (
-    <div className="properties">
-      {properties.map((property) => (
-        <div key={property.id} className="col">
-          <PropertyCard {...property} />
-        </div>
-      ))}
-      <Alert message={alert.message} success={alert.isSuccess} />
-    </div>
+    <>
+      <div>
+        <SideBar />
+      </div>
+      <div className="properties">
+        {properties.map((property) => (
+          <div key={property.id} className="col">
+            <PropertyCard {...property} />
+          </div>
+        ))}
+        <Alert message={alert.message} success={alert.isSuccess} />
+      </div>
+    </>
   );
 }
 
