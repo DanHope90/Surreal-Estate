c135eaba1e06ba367ff463adbaf8a89003c3d659
Dan Hope <dplhope@gmail.com>
2022-04-19 11:49:26 +0100

    app test udpated

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "../components/App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/surreal-estate/i);
-  expect(linkElement).toBeInTheDocument();
+  const { asFragment } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
