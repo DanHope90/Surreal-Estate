6e35b2ef01e094bb6eef0f2b40c1a254569dab1c
Dan Hope <dplhope@gmail.com>
2022-04-24 16:02:53 +0100

    facebook login and out added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
@@ -6,13 +6,28 @@ import Properties from "./Properties";
 import AddProperty from "./AddProperty";
 
 function App() {
+  const [userID, setUserId] = useState(false);
+
+  const onLogin = (response) => {
+    setUserId(response.userID);
+  };
+
+  const onLogout = () => {
+    window.FB.logout();
+    setUserId("");
+  };
+
   return (
     <Router>
       <div className="App">
-        <NavBar />
+        <NavBar userID={userID} onLogin={onLogin} onLogout={onLogout} />
       </div>
       <Switch>
-        <Route exact path="/" component={Properties} />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Properties {...props} userID={userID} />}
+        />
         <Route exact path="/add-property" component={AddProperty} />
       </Switch>
     </Router>
