fa25a3a8b701da64913649bfed7389be1af51e7c
Dan Hope <dplhope@gmail.com>
2022-04-24 16:01:21 +0100

    facebook sign in / out added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,42 @@
 import "../styles/NavBar.css";
-import { Link, React } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import FacebookLogin from "react-facebook-login";
+import PropTypes from "prop-types";
 import logo from "../logo.png";
 
-function NavBar() {
+function NavBar({ userID, onLogout, onLogin }) {
   return (
     <div className="navbar">
       <img className="logo" src={logo} alt="nav-logo" />
-      <ul>
-        <Link className="navbar-links-items" to="/">
+      <ul className="ul-list">
+        <Link className="navbar-links-main" to="/">
           View Properties
         </Link>
-        <Link className="navbar-links-items" to="/add-property">
+        <Link className="navbar-links-addProps" to="/add-property">
           Add Property
         </Link>
       </ul>
+      {userID ? (
+        <button type="submit" onClick={onLogout}>
+          Sign Out
+        </button>
+      ) : (
+        <FacebookLogin
+          appId="715135646329687"
+          autoLoad
+          callback={onLogin}
+          textButton="Login"
+        />
+      )}
     </div>
   );
 }
 
 export default NavBar;
+
+NavBar.propTypes = {
+  userId: PropTypes.number,
+  onLogin: PropTypes.func,
+  onLogout: PropTypes.func,
+}.isRequired;
